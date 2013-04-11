cce0942ea5ab93937cde7e882371e050583bf75b
jeremy <jduval@epsi.fr>
2013-04-11 22:16:55 +0200

    premier test
    verification

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jeremy
+ * Date: 11/04/13
+ * Time: 21:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
